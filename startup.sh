3f867a21849d103cb62f2eb984bf3768c0b9d4f0
Dr-Crow <James.Crowley1@marist.edu>
2019-07-27 18:57:52 -0400

    Debug

--- startup.sh
+++ startup.sh
@@ -74,6 +74,8 @@ counter=0
 
 echo "Waiting up to $MYSQL_DELAY seconds for MySQL. Checking every $MYSQL_CHECK_INTERVAL seconds."
 
+echo "mysql -h "$DB_ADDR" --port="$MYSQL_PORT" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" -e "show databases;""
+
 while ! mysql -h "$DB_ADDR" --port="$MYSQL_PORT" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" -e "show databases;" > /dev/null 2>&1; do
     if [ $counter -gt $MYSQL_DELAY ]; then
         >&2 echo "We have been waiting for MySQL too long already; failing."
